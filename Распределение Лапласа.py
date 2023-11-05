f39393c413ca5e7b6ebdbb3044bb3a23509d6eb7
Danzar1991 <101476478+Danzar1991@users.noreply.github.com>
2023-11-05 21:41:20 +0300

    initial commit

---
+++ Распределение Лапласа.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        # Your code here
+        ####
+        return np.mean(np.abs(x - np.median(x)))
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        self.features = features
+        self.loc = np.mean(features)
+        self.scale = np.sqrt(2 * np.mean(np.abs(features - np.median(features))))
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return -np.log(2 * self.scale) - np.abs(values - self.loc) / self.scale
+
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(values))
